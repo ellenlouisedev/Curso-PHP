6b405b5ec0a86192af4693187b5877bc3134dcb2
ellenlouisedev <ellenlouise767@gmail.com>
2024-10-20 21:16:58 -0300

    Criei o repositório

---
+++ ex003/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        // 0x = Hexadecimal, 0b = Binário 0 = Octal
+        // $num = "0x1A";
+        // echo"O valor da variável é $num";
+
+        // $v = "Ellen";
+        // var_dump($v);
+
+        // $num = (int) 3e2; // 3 x 10(2)
+        // echo "O valor é $num";
+        // var_dump($num);
+
+        // $num = (int) "950";
+        // var_dump($num);
+
+        $casado = true;
+        // var_dump($casado);
+        echo "O valor para casado é $casado";
+    ?>
+</body>
+</html>
