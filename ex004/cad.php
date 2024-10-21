4db3e44435fd20b69b76eb29f838471814785d83
ellenlouisedev <ellenlouise767@gmail.com>
2024-10-20 22:30:36 -0300

    :P

--- ex004/cad.php
+++ ex004/cad.php
@@ -13,10 +13,11 @@
     <main>
         <?php
             // var_dump($_GET); // $_GET $_POST $_COOKIES
-            $nome = $_GET["nome"];
-            $sobrenome = $_GET["sobrenome"];
-            echo "É um prazer te conhecer, $nome $sobrenome! Este é o meu site. aaaaa";
+            $nome = $_GET["nome"] ?? "sem nome";
+            $sobrenome = $_GET["sobrenome"] ?? "sem sobrenome";
+            echo "É um prazer te conhecer, <b>$nome $sobrenome</b>! Este é o meu site.";
         ?>
+        <p><a href="javascript:history.go(-">Voltar para a página anterior</a></p>
     </main>
 </body>
 </html>
